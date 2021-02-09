a2dc8186ebe933355f792d978610bd287febfd6b
Olavi Koistinen <kolvi@iki.fi>
2021-02-09 22:12:25 +0200

    tidy tidy

--- lib/luhistin/text_distorter/double_letters.rb
+++ lib/luhistin/text_distorter/double_letters.rb
@@ -7,12 +7,18 @@ module Luhistin
 
         modify_some_words(text) do |word|
           if (word.length > 3)
-            dup_point = word.random_cut_point
-            word.insert(dup_point, word[dup_point])
+            duplicate_some_letter(word)
           else
             word
           end
         end
+      end
+
+      private
+
+      def duplicate_some_letter(word)
+        selected_letter_indx = word.random_cut_point
+        word.insert( selected_letter_indx, word[selected_letter_indx] )
       end
 
     end
