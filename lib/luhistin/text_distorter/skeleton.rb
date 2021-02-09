450b1cba688b9daa89a8c9559e24fbe5f41a3053
Olavi Koistinen <kolvi@iki.fi>
2021-02-09 20:33:33 +0200

    Simplifying further

--- lib/luhistin/text_distorter/skeleton.rb
+++ lib/luhistin/text_distorter/skeleton.rb
@@ -14,9 +14,10 @@ module Luhistin
 
 			locations.reverse.each do |space|
 			  relative_position = (space.to_f) / (text.length)
-			  propability = propability_from_curve(relative_position)
+			  #propability = propability_from_curve(relative_position)
 
-			  if randomly_selected?(propability)
+			  #if randomly_selected?(propability)
+			  if randomly_selected?(relative_position)
 			  	text.slice! space
 			  end
 			end
@@ -28,8 +29,9 @@ module Luhistin
       # want to modify text word-by-word basis
 
 	  def modify_some_words(text)
-	  	traverse_each_word(text) do |word, propability|
-	  	  if randomly_selected? (propability)
+	  	#traverse_each_word(text) do |word, propability|
+	  	traverse_each_word(text) do |word, relative_position|
+	  	  if randomly_selected? (relative_position)
 	  	  	yield(word)
 	  	  else
 	  	  	word
@@ -41,9 +43,11 @@ module Luhistin
 		lines = text.wordlines
 		res = lines.map.with_index do |line, li|
 		    line.map.with_index do |word, wi|
-      		  propability = propability_from_curve( lines.relative_position_of_word(li, wi) )
+      		  #propability = propability_from_curve( lines.relative_position_of_word(li, wi) )
+      		  relative_position = lines.relative_position_of_word(li, wi)
 
-		      yield(word, propability)
+		      #yield(word, propability)
+		      yield(word, relative_position)
 
 		    end
 		  end
@@ -53,12 +57,14 @@ module Luhistin
       # Find the "bias" (todo: think better names)
       # for randomly determining if the distortion should happen
 
-	  def propability_from_curve(x_point)
-	    @curve[(@curve.length-1)*x_point]
+      #def randomly_selected? (propability)
+      def randomly_selected? (relative_position)
+      	Random.rand < propability_from_curve( relative_position )
+      	#Random.rand < propability
       end
 
-      def randomly_selected? (propability)
-      	Random.rand < propability
+	  def propability_from_curve(x_point)
+	    @curve[(@curve.length-1)*x_point]
       end
 
     end
