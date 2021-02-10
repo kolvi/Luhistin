4009d960841c05cf906b3de1d9e56af7490b3bcf
Olavi Koistinen <kolvi@iki.fi>
2021-02-10 10:24:12 +0200

    Remove unused code

--- lib/core_extensions/string/string.rb
+++ lib/core_extensions/string/string.rb
@@ -2,11 +2,4 @@ class String
   def random_cut_point
     rand(1..(length-1))
   end
-
-  def indexes_of(chr)
-    index_list = [[]]
-    (index_list + split(Regexp.new(chr))[0..-2]).inject do |indexes, str|
-      indexes.empty? ? [str.length] : (indexes + [indexes.last+str.length+1])
-    end
-  end
 end
