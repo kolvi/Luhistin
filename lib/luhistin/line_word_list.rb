63e894124aa488621a1835d4e0b77a0af177f5b1
Olavi Koistinen <kolvi@iki.fi>
2021-02-10 10:09:23 +0200

    Tidying

--- lib/luhistin/line_word_list.rb
+++ lib/luhistin/line_word_list.rb
@@ -6,18 +6,6 @@ module Luhistin
   		@word_grid = create_word_grid(str)
   	end
 
-	def relative_position_of_word(line_ind, word_ind)
-	    return 0 if @word_grid.flatten.length == 0 # border case: empty array
-	    flattened_index = if (line_ind == 0)
-	    	word_ind
-	    else
-	    	no_of_nested_items(line_ind-1) + word_ind
-	    end
-
-	    # Return "relative position" of word = float between 0 and 1)
-	    flattened_index.to_f / @word_grid.flatten.length
-	end
-
 	#
 	# Service function for browsing list word by word, modifying
 	# the words as necessary and getting result (the entire
@@ -37,6 +25,21 @@ module Luhistin
 	end
 
   	private
+
+  	# Note: This could have some use a public function, but
+  	# currently no other class needs it
+
+	def relative_position_of_word(line_ind, word_ind)
+	    return 0 if @word_grid.flatten.length == 0 # border case: empty array
+	    flattened_index = if (line_ind == 0)
+	    	word_ind
+	    else
+	    	no_of_nested_items(line_ind-1) + word_ind
+	    end
+
+	    # Return "relative position" of word = float between 0 and 1)
+	    flattened_index.to_f / @word_grid.flatten.length
+	end
 
   	def revert_to_string(wordlines)
   		wordlines.map { |words_in_line| words_in_line.join(" ") }.join("\n")
