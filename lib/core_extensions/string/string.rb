449fd67e39817604f2a81628373cb853dc8bbff7
Olavi Koistinen <kolvi@iki.fi>
2021-02-10 10:26:45 +0200

    Comments

--- lib/core_extensions/string/string.rb
+++ lib/core_extensions/string/string.rb
@@ -1,4 +1,11 @@
 class String
+
+  # Gives a random point in the middle of a word.
+  # TODO: Does it actually do that? Debug
+  #
+  # We carefully choose to monkey-patch String class
+  # with this function, since it leads to quite elegant code.
+  #
   def random_cut_point
     rand(1..(length-1))
   end
