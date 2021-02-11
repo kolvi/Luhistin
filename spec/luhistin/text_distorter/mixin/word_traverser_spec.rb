fe5fd7401b450f74c0f338509c82a1051a0fd0ba
Olavi Koistinen <kolvi@iki.fi>
2021-02-11 17:58:23 +0200

    More simple tests

--- spec/luhistin/text_distorter/mixin/word_traverser_spec.rb
+++ spec/luhistin/text_distorter/mixin/word_traverser_spec.rb
@@ -29,5 +29,14 @@ describe Luhistin::TextDistorter::SpecClassWordTraverser do
     expect(result).to eq("---Here+++ are ---some+++ words")
   end
 
+  it 'does not panic if string is empty' do
+    expect(subject.modify_some_words("")).to eq("")
+  end
+
+  it 'does not fail for one word string' do
+    allow(Random).to receive(:rand).and_return(0.9)
+    expect(subject.modify_some_words("hello")).to eq("hello")
+  end
+
 end
 
