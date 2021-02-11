92bcf554f053924cda7681006d3164e3bab7de55
Olavi Koistinen <kolvi@iki.fi>
2021-02-11 17:51:28 +0200

    Adding word traverser test, just one

---
+++ spec/luhistin/text_distorter/mixin/word_traverser_spec.rb
@@ -0,0 +1,31 @@
+require 'luhistin'
+
+module Luhistin
+  module TextDistorter
+    class SpecClassWordTraverser < CurveDistorter
+      include WordTraverser
+    end
+  end
+end
+
+describe Luhistin::TextDistorter::SpecClassWordTraverser do
+
+  before(:each) do
+    # Note: We could mock PropabilityCurve, but the result is
+    # deterministic and really quite simple, so we decide not to
+
+    subject.curve = Luhistin::PropabilityCurve.new([0.5])
+  end
+
+  it 'traverses a short string word, calling block for modifications' do
+    allow(Random).to receive(:rand).and_return(*[0.2, 0.8]*999)
+
+    text = "Here are some words"
+    result = subject.modify_some_words(text) do |word|
+      "---#{word}+++"
+    end
+    expect(result).to eq("---Here+++ are ---some+++ words")
+  end
+
+end
+
