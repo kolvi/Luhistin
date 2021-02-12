9da228c51f268529405e99d61d2db4efb8fbedd3
Olavi Koistinen <kolvi@iki.fi>
2021-02-12 16:37:56 +0200

    More tezxzts

--- spec/luhistin/text_tool/word_grid_traverser_spec.rb
+++ spec/luhistin/text_tool/word_grid_traverser_spec.rb
@@ -9,11 +9,6 @@ describe Luhistin::TextTool::WordGridTraverser do
       ["please!"],
       ["Simple", "word."]
     ]
-
-    result = subject.traverse(grid) do |word, rel_position|
-      [word, rel_position]
-    end
-
     expected_result = [
       [["A", 0.0], ["word", 0.125]],
       [["for", 0.25], ["you,", 0.375], ["sir", 0.5]],
@@ -21,10 +16,34 @@ describe Luhistin::TextTool::WordGridTraverser do
       [["Simple", 0.75], ["word.", 0.875]],
     ]
 
+    result = subject.traverse(grid) do |word, rel_position|
+      [word, rel_position]
+    end
+
     expect(result).to eq(expected_result)
   end
+
   it "handles an empty list graciously" do
     expect(subject.traverse([])).to eq([])
+  end
+
+  it "handles empty rows graciously" do
+    grid = [
+      ["A", "word"],
+      [],
+      ["for","you,","sir"],
+    ]
+    expected_result = [
+      [["A", 0.0], ["word", 0.2]],
+      [],
+      [["for", 0.4], ["you,", 0.6], ["sir", 0.8]]
+    ]
+
+    result = subject.traverse(grid) do |word, rel_position|
+      [word, rel_position]
+    end
+
+    expect(result).to eq(expected_result)
   end
 
 end
