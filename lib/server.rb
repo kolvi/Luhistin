f6097d6202c506bd740fdcd8fe2ce8dedfacf00c
Olavi Koistinen <kolvi@iki.fi>
2021-02-15 20:11:49 +0200

    Still more cleanup

--- lib/server.rb
+++ lib/server.rb
@@ -1,10 +1,5 @@
 require_relative 'routes/sourcetexts'
 
-#set :public_folder, __dir__ + '/../client/dist'
-#set :views, File.dirname(__FILE__) + '/views'
-#set :views, __dir__ + '/../views'
-#set :views, File.dirname(__FILE__) + '../views'
-
 class LuhistinServer < Sinatra::Base
 	get '/' do
 		erb :index
