73003db9c20ca02ea939d9deaaabd26318cb465a
Olavi Koistinen <kolvi@iki.fi>
2021-02-15 23:46:44 +0200

    Route save

--- lib/routes/sourcetexts.rb
+++ lib/routes/sourcetexts.rb
@@ -15,49 +15,51 @@ class LuhistinServer < Sinatra::Base
   end
 
   def ensure_parameters_are_keys_for (model_class, parameter_hash)
-    # Validate:
-    # 1. Are all columns actually columns of said class?
-    # 2. Is id included? (Shouldn't be!)
+    # Ensude that:
+    # 1. all parameters are actually columns of the class
+    # 2. "id" is not one of them!
     available_keys = model_class.columns-[:id]
     halt 400 unless (parameter_hash.keys - available_keys).empty?
   end
 
-  get '/sourcetext/:id' do
-    stext = sourcetext_by_id
+#  def success_with_sourcetext(stext_id)
+#    status 200
+#    Sourcetext[stext_id].values.to_json
+#  end
+
+  def success_with_sourcetext(stext)
     status 200
     stext.values.to_json
   end
 
+  get '/sourcetext/:id' do
+    success_with_sourcetext( sourcetext_by_id )
+  end
+
   post '/sourcetext' do
     body_params = read_bodypars
-    ensure_parameters_are_keys_for Sourcetext, body_params
+    ensure_parameters_are_keys_for(Sourcetext, body_params)
     begin
-      new_id = Sourcetext.insert body_params
+      stext = Sourcetext.create(body_params)
     rescue Sequel::Error
       halt 400
     end
-    status 200
-    Sourcetext[new_id].values.to_json
+    success_with_sourcetext stext
   end
 
 
   patch '/sourcetext/:id' do
     begin
-       stext = sourcetext_by_id
        body_params = read_bodypars
-
-       ensure_parameters_are_keys_for Sourcetext, body_params
+       ensure_parameters_are_keys_for(Sourcetext, body_params)
 
        # Set all keys
-       stext.set_fields(body_params, body_params.keys).save
+       (sourcetext_by_id).set_fields(body_params, body_params.keys).save
 
-    #  was [Sequel::DatabaseError, Sequel::ConstraintViolation, Sequel::ValidationFailed, JSON::ParserError]
-    #rescue *[Sequel::Error, JSON::ParserError]
     rescue Sequel::Error
        halt 400
     end
-     status 200
-     stext.values.to_json
+     success_with_sourcetext( Sourcetext[params[:id]] )
   end
 
   delete '/sourcetext/:id' do
