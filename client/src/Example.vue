e64e0cccf68cd41363685be18015389fb7057ebf
Olavi Koistinen <kolvi@iki.fi>
2021-02-15 09:05:27 +0200

    Fixed webpack config

--- client/src/Example.vue
+++ client/src/Example.vue
@@ -6,7 +6,7 @@
 export default {
   data () {
     return {
-      msg: 'Hello world! Rautaa rajalle'
+      msg: 'Hello world! Rautaa rajalle ny!'
     }
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './client/src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'client/dist'),
   },
   devServer: {
     contentBase: './client/dist',
