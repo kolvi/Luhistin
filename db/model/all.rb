49d04b1f1b3c622aa88c88c9fcb0d801bb1bd622
Olavi Koistinen <kolvi@iki.fi>
2021-02-16 23:08:07 +0200

    More Sinatra init

--- db/model/all.rb
+++ db/model/all.rb
@@ -1,17 +1,20 @@
 require 'dotenv'
-require 'sequel'
+#require 'sequel'
 
 Dotenv.load
 
-Sequel.extension :pg_json_ops
+#Sequel.extension :pg_json_ops
 DB = Sequel.connect(adapter: :postgres,
   	user: ENV['DB_USER'],
   	password: ENV['DB_PASSWORD'],
   	database: 'luhistin')
+
+DB.extension :pg_array, :pg_row
+DB.register_row_type(:distortion)
 
 Sequel::Model.db = DB
 
 require_relative 'recipe'
 require_relative 'textsource'
 
-require 'pry'; binding.pry
+#require 'pry'; binding.pry
--- lib/server.rb
+++ lib/server.rb
@@ -1,5 +1,9 @@
 require_relative 'routes/sourcetexts'
 
+# Initialising Sinatra stuff
+
+Sequel.extension :core_extensions
+
 class LuhistinServer < Sinatra::Base
 	get '/' do
 		erb :index
